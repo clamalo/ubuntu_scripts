d9bf0714f65a57d36b7f4e6db3745f4a3d974b02
clamalo <89725263+clamalo@users.noreply.github.com>
2022-04-27 16:08:17 -0700

    Update href_ubuntu.py

--- href_ubuntu.py
+++ href_ubuntu.py
@@ -558,8 +558,8 @@ def ingest_gribs(frame,master_ds):
     frame = name_frame(int(frame))
     datestr = datestr_and_cycle()[0]
     cycle = datestr_and_cycle()[1]
-    resolutions = [3,5,2.5]
-    # resolutions = [2.5]
+    # resolutions = [3,5,2.5]
+    resolutions = [3]
     for resolution in resolutions:
         if resolution == 3:
             #load downscaling file
