927f03354bb5e356089c3e9c91449ddeda06ef0c
clamalo <89725263+clamalo@users.noreply.github.com>
2022-04-27 00:34:23 -0700

    Update href_ubuntu.py

--- href_ubuntu.py
+++ href_ubuntu.py
@@ -168,7 +168,7 @@ def nam3k(chelsa_ds,frame,cycle,datestr,offset):
     idx_file = '/root/nam.t'+cycle+'z.conusnest.hiresf'+frame+'.tm00.grib2.idx'
     read_idx(idx_file,'nam',int(frame),cycle,datestr)
     (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-    os.system('gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
+    os.system('/root/anaconda3/envs/blend/bin/gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
     inputfile = 'current_.tif'
     outputfile = 'current_.nc'
     ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -184,7 +184,7 @@ def nam3k(chelsa_ds,frame,cycle,datestr,offset):
         idx_file = '/root/nam.t'+cycle+'z.conusnest.hiresf'+frame+'.tm00.grib2.idx'
         read_idx(idx_file,'nam',int(frame),cycle,datestr)
         (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-        os.system('gdalwarp -t_srs EPSG:4326 current.nc minus_one_.tif')
+        os.system('/root/anaconda3/envs/blend/bin/gdalwarp-t_srs EPSG:4326 current.nc minus_one_.tif')
         inputfile = 'minus_one_.tif'
         outputfile = 'minus_one_.nc'
         ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -217,7 +217,7 @@ def hrrr3k(chelsa_ds,frame,cycle,datestr,offset):
     idx_file = '/root/hrrr.t'+cycle+'z.wrfsfcf'+frame+'.grib2.idx'
     read_idx(idx_file,'hrrr',int(frame),cycle,datestr)
     (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-    os.system('gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
+    os.system('/root/anaconda3/envs/blend/bin/gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
     inputfile = 'current_.tif'
     outputfile = 'current_.nc'
     ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -249,7 +249,7 @@ def arw5k_1(chelsa_ds,frame,cycle,datestr,offset):
     idx_file = '/root/hiresw.t'+cycle+'z.arw_5km.f'+frame+'.conus.grib2.idx'
     read_idx(idx_file,'arw5k_1',int(frame),cycle,datestr)
     (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-    os.system('gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
+    os.system('/root/anaconda3/envs/blend/bin/gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
     inputfile = 'current_.tif'
     outputfile = 'current_.nc'
     ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -281,7 +281,7 @@ def arw5k_2(chelsa_ds,frame,cycle,datestr,offset):
     idx_file = '/root/hiresw.t'+cycle+'z.arw_5km.f'+frame+'.conusmem2.grib2.idx'
     read_idx(idx_file,'arw5k_2',int(frame),cycle,datestr)
     (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-    os.system('gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
+    os.system('/root/anaconda3/envs/blend/bin/gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
     inputfile = 'current_.tif'
     outputfile = 'current_.nc'
     ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -313,7 +313,7 @@ def fv35k(chelsa_ds,frame,cycle,datestr,offset):
     idx_file = '/root/hiresw.t'+cycle+'z.fv3_5km.f'+frame+'.conus.grib2.idx'
     read_idx(idx_file,'fv35k',int(frame),cycle,datestr)
     (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-    os.system('gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
+    os.system('/root/anaconda3/envs/blend/bin/gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
     inputfile = 'current_.tif'
     outputfile = 'current_.nc'
     ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -345,7 +345,7 @@ def arw2p5k(chelsa_ds,frame,cycle,datestr,offset):
     idx_file = '/root/hiresw.t'+cycle+'z.arw_2p5km.f'+frame+'.conus.grib2.idx'
     read_idx(idx_file,'arw2p5k',int(frame),cycle,datestr)
     (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-    os.system('gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
+    os.system('/root/anaconda3/envs/blend/bin/gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
     inputfile = 'current_.tif'
     outputfile = 'current_.nc'
     ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -378,7 +378,7 @@ def fv32p5k(chelsa_ds,frame,cycle,datestr,offset):
     idx_file = '/root/hiresw.t'+cycle+'z.fv3_2p5km.f'+frame+'.conus.grib2.idx'
     read_idx(idx_file,'fv32p5k',int(frame),cycle,datestr)
     (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-    os.system('gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
+    os.system('/root/anaconda3/envs/blend/bin/gdalwarp -t_srs EPSG:4326 current.nc current_.tif')
     inputfile = 'current_.tif'
     outputfile = 'current_.nc'
     ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -505,7 +505,7 @@ def create_master_ds():
             idx_file = '/root/nam.t'+cycle+'z.conusnest.hiresf'+frame+'.tm00.grib2.idx'
             read_idx(idx_file,'nam',int(frame),cycle,datestr)
             (xr.load_dataset('current.grib2')).to_netcdf('current.nc')
-            os.system('gdalwarp -t_srs EPSG:4326 current.nc master.tif')
+            os.system('/root/anaconda3/envs/blend/bin/gdalwarp-t_srs EPSG:4326 current.nc master.tif')
             inputfile = 'master.tif'
             outputfile = 'master.nc'
             ds = gdal.Translate(outputfile, inputfile, format='NetCDF')
@@ -514,7 +514,7 @@ def create_master_ds():
             if 'crs' in str(dataset):
                 dataset = dataset.drop(['crs'])
 
-        # os.system('gdalwarp -t_srs EPSG:4326 current.grib2 current_.grib2')
+        # os.system('/root/anaconda3/envs/blend/bin/gdalwarp-t_srs EPSG:4326 current.grib2 current_.grib2')
         # dataset = xr.load_dataset('/root/current_.grib2',engine='cfgrib')
         dataset['lon'] = dataset['lon']+360
         dataset = crop_ds(dataset,'180_chelsa')
