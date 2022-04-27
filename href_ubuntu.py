505ac566f355ccf10f556ada1ea4a516d48d560e
clamalo <89725263+clamalo@users.noreply.github.com>
2022-04-27 14:29:02 -0700

    Update href_ubuntu.py

--- href_ubuntu.py
+++ href_ubuntu.py
@@ -770,7 +770,7 @@ frame = '03'
 master_master_ds = create_master_ds()
 # master_ds = create_master_ds()
 # print(master_ds)
-for n in range(2,36):
+for n in range(8,36):
     master_ds = create_master_ds()
     frame = name_frame(n)
     master_ds = ingest_gribs(frame,master_ds)
