8c1b969631ae862abfd695017209f3021ba3c588
clamalo <89725263+clamalo@users.noreply.github.com>
2022-04-27 22:04:34 -0700

    Update href_ubuntu.py

--- href_ubuntu.py
+++ href_ubuntu.py
@@ -843,8 +843,8 @@ for n in range(2,36):
     # master_ds['tp'] = (master_ds['nam3k']+master_ds['hrrr3k']+master_ds['arw5k_1']+master_ds['arw5k_2']+master_ds['fv35k']+master_ds['arw2.5k']+master_ds['fv32.5k'])/7
     # master_ds['tp'] = (master_ds['nam3k_1']+master_ds['nam3k_2']+master_ds['nam3k_3']+master_ds['nam3k_4']+master_ds['nam3k_5']+master_ds['hrrr3k_1']+master_ds['hrrr3k_2']+master_ds['hrrr3k_3']+master_ds['arw5k_1_1']+master_ds['arw5k_1_2']+master_ds['arw5k_2_1']+master_ds['arw5k_2_2']+master_ds['fv35k_1']+master_ds['fv35k_2']+master_ds['fv35k_3']+master_ds['arw2.5k_1']+master_ds['arw2.5k_2']+master_ds['fv32.5k_1']+master_ds['fv32.5k_2']+master_ds['fv32.5k_3'])/20
     # master_ds['tp'] = (master_ds['nam3k_1']+master_ds['nam3k_2']+master_ds['hrrr3k_1']+master_ds['hrrr3k_2'])/4
-    master_ds['tp'] = (master_ds['nam3k_1']+master_ds['hrrr3k_1'])/2
-    # master_ds['tp'] = (master_ds['nam3k_1']+master_ds['hrrr3k_1']+master_ds['arw5k_1_1']+master_ds['arw5k_2_1']+master_ds['fv35k_1']+master_ds['arw2.5k_1']+master_ds['fv32.5k_1'])/7
+    # master_ds['tp'] = (master_ds['nam3k_1']+master_ds['hrrr3k_1'])/2
+    master_ds['tp'] = (master_ds['nam3k_1']+master_ds['hrrr3k_1']+master_ds['arw5k_1_1']+master_ds['arw5k_2_1']+master_ds['fv35k_1']+master_ds['arw2.5k_1']+master_ds['fv32.5k_1'])/7
     master_ds.to_netcdf('/root/master_ds.nc')
     print(master_ds)
 
@@ -880,7 +880,7 @@ for n in range(2,36):
     # elif int(frame) == 9:
     #     tp = ds['tp']*.0393701
 
-    tp = ds['nam3k_1']*.0393701
+    tp = ds['arw5k_1_1']*.0393701
 
     fig = plt.figure(figsize=(12, 8))
     ax = plt.axes(projection=ccrs.PlateCarree())
